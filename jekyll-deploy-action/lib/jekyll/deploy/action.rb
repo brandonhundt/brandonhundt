059f8c31af4b6c31be8b090c2faa2cb52160a56e
Brandon Hundt <brandon.hundt@prx.org>
2021-04-13 17:30:23 -0500

    install jekyll deploy action

---
+++ jekyll-deploy-action/lib/jekyll/deploy/action.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative "action/version"
+
+module Jekyll
+  module Deploy
+    module Action
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
