542a57572f79ce4322f956719b510e66ae15ddee
Brandon Hundt <brandon.hundt@prx.org>
2023-09-27 23:10:25 -0500

    more styling on the new theme

--- _site/assets/js/scripts.js
+++ _site/assets/js/scripts.js
@@ -1,21 +1,30 @@
-// Function to handle changes in intersection
-function handleIntersection(entries, observer) {
-    entries.forEach(function (entry) {
-      if (entry.isIntersecting) {
-        document.body.setAttribute('data-theme', entry.target.getAttribute('data-color'));
+window.addEventListener("scroll", function() {
+    // selectors
+    var body = document.body,
+        panels = document.querySelectorAll('[data-color]');
+  
+    // Calculate the offset to start changing colors (33%)
+    var offset = window.innerHeight / 3;
+  
+    // Iterate through each panel
+    panels.forEach(function (panel) {
+      var rect = panel.getBoundingClientRect();
+  
+      // Calculate the top and bottom scroll positions for this panel
+      var panelTop = rect.top + window.pageYOffset - offset; // Adjusted with offset
+      var panelBottom = panelTop + rect.height;
+  
+      // Check if the current scroll position is within the panel's range
+      if (window.pageYOffset >= panelTop && window.pageYOffset < panelBottom) {
+        // Remove all classes on body with color-
+        body.className = body.className.replace(/\bcolor-\S+/g, '');
+  
+        // Add class of the currently active div
+        body.classList.add('color-' + panel.getAttribute('data-color'));
       }
     });
-  }
-  
-  // Create an Intersection Observer
-  var observer = new IntersectionObserver(handleIntersection, {
-    threshold: 0.33, // Trigger when 33% of the target is visible
   });
   
-  // Select all panels with the class 'scroll-bg'
-  var panels = document.querySelectorAll('[data-color]');
+  // Trigger the scroll event once to apply the initial state
+  window.dispatchEvent(new Event('scroll'));
   
-  // Observe each panel
-  panels.forEach(function (panel) {
-    observer.observe(panel);
-  });
